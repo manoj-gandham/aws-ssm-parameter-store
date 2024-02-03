a047f35001308ba9f851b2ff69649e0e31deb696
manoj <gandhamanojkutty@gmail.com>
2024-02-03 14:17:54 +0530

    code updated

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "aws_ssm_parameter" "parameters" {
   count = length(var.parameters)
   name  = var.parameters[count.index].name
-  type  = var.parameters[count.index].value
-  value = "helo from terraform code"
+  value  = var.parameters[count.index].value
+  type = "string"
   key_id = "e8f1906a-a356-4f85-9dca-28dd3394a727"
 }
