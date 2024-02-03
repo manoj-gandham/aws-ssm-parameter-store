acb238fd901b4972b0a8583dd9cca3fce4c0dde6
manoj <gandhamanojkutty@gmail.com>
2024-02-03 14:12:55 +0530

    code updated

---
+++ env-dev/main.tfvars
@@ -0,0 +1,6 @@
+parameters = [
+  { name = "dev.frontend.catalogue-url", value = "http://catalogue-dev.mdevops333.online:8080/"},
+  { name = "dev.frontend.cart-url", value = "http://cart-dev.mdevops333.online:8080/"}
+
+
+]
--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
-resource "aws_ssm_parameter" "parameter" {
-  name  = "dev.test1"
-  type  = "SecureString"
+resource "aws_ssm_parameter" "parameters" {
+  count = length(var.parameters)
+  name  = var.parameters[count.index].name
+  type  = var.parameters[count.index].value
   value = "helo from terraform code"
   key_id = "e8f1906a-a356-4f85-9dca-28dd3394a727"
 }
