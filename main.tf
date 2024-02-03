5e8dfdc731323a95d9d489bf909154ad00d974af
manoj <gandhamanojkutty@gmail.com>
2024-02-03 13:35:08 +0530

    code updated

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 resource "aws_ssm_parameter" "parameter" {
   name  = "dev.test1"
-  type  = "String"
+  type  = "SecureString"
   value = "helo from terraform code"
+  key_id = "e8f1906a-a356-4f85-9dca-28dd3394a727"
 }
