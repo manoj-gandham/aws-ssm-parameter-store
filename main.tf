d8dd3bc5b8a71c5ce1a01cfe4f3ee09b2617b0db
manoj <gandhamanojkutty@gmail.com>
2024-02-05 23:21:23 +0530

    code updated

--- main.tf
+++ main.tf
@@ -5,3 +5,14 @@ resource "aws_ssm_parameter" "parameters" {
   type = "String"
   key_id = "e8f1906a-a356-4f85-9dca-28dd3394a727"
 }
+
+resource "aws_ssm_parameter" "passwords" {
+  count = length(var.passwords)
+  name  = var.passwords[count.index].name
+  value  = var.passwords[count.index].value
+  type = "SecureString"
+  key_id = "e8f1906a-a356-4f85-9dca-28dd3394a727"
+}
+
+
+
